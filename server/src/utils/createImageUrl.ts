0718aa334ba092054f01913670836e2a6234d1ec
Yevgenii Oliinyk <oliinykyevgenii@gmail.com>
2023-09-25 15:14:22 +0300

    files uploads

--- server/src/utils/createImageUrl.ts
+++ server/src/utils/createImageUrl.ts
@@ -1,20 +1,9 @@
-// import fs from 'fs';
-import logger from './logger.js';
-// import logger from './logger.js';
-
 const BASE_API = process.env.BASE_API_URL as string
 
 export const urlList = (fileList: Express.Multer.File[]) => {
     const urls = fileList.map((file) => {
         const { path } = file
         const newPath = '/' + path
-        logger.info(newPath)
-        // fs.renameSync(path, newPath)
-        // const { originalname, path } = file
-        // const parts = originalname.split('.')
-        // const ext = parts[parts.length - 1]
-        // const newPath = path + '.' + ext
-        // logger.info(newPath)
 
         return { link: BASE_API + newPath }
     })
--- server/src/utils/deleteFiles.ts
+++ server/src/utils/deleteFiles.ts
@@ -3,9 +3,11 @@ import { __dirname } from '../app.js'
 import logger from './logger.js';
 import { IImagesLinksList } from '../types/Types.js';
 
+const BASE_API_URL = process.env.BASE_API_URL as string
+
 export const deleteFiles = (urlList: IImagesLinksList[]) => {
     urlList.forEach(item => {
-        const imagePath = item.link?.replace('http://localhost:4000/', __dirname) as string
+        const imagePath = item.link?.replace(BASE_API_URL, __dirname) as string
         fs.unlink(imagePath, (err) => {
             if (err) {
                 logger.info(err)
